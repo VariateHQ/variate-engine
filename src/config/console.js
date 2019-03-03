f34a20158017c14fab8a2aa853133d8cff4bd0de
Valentin Prugnaud <valentin@whatdafox.com>
2019-03-03 05:30:26 -0800

    Support for dark modes

--- src/config/console.js
+++ src/config/console.js
@@ -9,5 +9,5 @@ export const styles = {
     error: "background: #e17055; color: white; font-weight: 500; border-radius: 3px 0 0 3px; padding: 1px 2px;",
     warning: "background: #fdcb6e; color: white; font-weight: 500; border-radius: 3px 0 0 3px; padding: 1px 2px;",
     type: "background: #424242; color: white; font-weight: 400; padding: 1px 2px; border-radius: 0 3px 3px 0;",
-    message: "background: transparent; color: #424242; font-weight: 400;"
+    message: "background: transparent; font-weight: 400;"
 }
